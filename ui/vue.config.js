c6f775f2b36bfaeba34927f877ad7d73b4b5fd53
Bobby Wettig <bobbywettig@gmail.com>
2022-03-11 22:13:16 -0700

    output dir

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "prooduction" ? "/REPO_NAME/" : "/"
+    publicPath: process.env.NODE_ENV === "prooduction" ? "/REPO_NAME/" : "/",
+    outputDir: "docs",
 }
